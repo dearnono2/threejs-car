784b889dd767266ea500f22e40a4fa217cff5e6b
dearnono2 <dearnono2@gmail.com>
2022-11-14 14:16:48 +0900

    서브 텍스트 모션 활성화

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
 
 	return (
 		<figure>
+			<h1>Dcode Lab</h1>
 			<Canvas shadowMap style={{ background: '#cdcfd3' }} camera={{ position: [7, 7, 7] }}>
 				<Orbit />
 				{/* <axesHelper args={[5]} /> */}
--- src/components/Txt.js
+++ src/components/Txt.js
@@ -28,21 +28,67 @@ function Txt({ index }) {
     <section className='txt' ref={txt}>
       <article>
         <h2>Consect</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio nihil
+          totam minus, expedita debitis quas! Reiciendis distinctio in fugit
+          molestiae.
+        </p>
       </article>
+
       <article>
         <h2>Dolor</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sit
+          enim obcaecati. <br />
+          <br />
+          Rem id saepe sapiente provident. Itaque dicta explicabo, minima
+          assumenda corporis nemo placeat optio quam consectetur! Vel ad
+          praesentium aspernatur doloremque quae maxime ipsum exercitationem
+          magnam ipsam voluptas?
+        </p>
       </article>
+
       <article>
         <h2>Lorem</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nisi
+          suscipit enim officiis porro, eligendi excepturi repudiandae corrupti
+          molestiae.
+        </p>
       </article>
+
       <article>
         <h2>Sit amet</h2>
+        <p>
+          Necessitatibus eos maxime eaque quaerat iusto est modi temporibus.
+          <br />
+          <br />
+          Aliquid quaerat accusamus amet blanditiis deserunt eaque, labore nobis
+          tempore facere minus inventore totam quo nulla doloremque deleniti
+          dolore qui officia consequatur corrupti. <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, quod!
+        </p>
       </article>
+
       <article>
         <h2>Adipisic</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br />
+          <br />
+          /Soluta deserunt quam impedit, sunt atque similique expedita accusamus
+          omnis, pariatur debitis, perspiciatis adipisci exercitationem natus
+          illum ratione neque id placeat voluptas sed minima odio sint.
+        </p>
       </article>
+
       <article>
         <h2> Corporis</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br />
+          Minima iste quo doloremque veritatis assumenda quam reiciendis! Et
+          pariatur delectus dicta asperiores, iste, modi doloribus architecto
+          rerum a omnis molestias.
+        </p>
       </article>
     </section>
   );
