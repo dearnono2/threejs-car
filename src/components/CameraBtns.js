96d77f71b6122f07ccd44f685d2dce87c8a297a1
dearnono2 <dearnono2@gmail.com>
2022-11-14 12:11:35 +0900

    클릭 버튼 활성화

--- src/components/CameraBtns.js
+++ src/components/CameraBtns.js
@@ -1,6 +1,9 @@
 import state from '../state';
+import { useRef } from 'react';
 
 function CameraBtns({ setIndex }) {
+  const btns = useRef(null);
+
   const pos = {
     0: {
       cameraPos: [7, 0, 7],
@@ -29,6 +32,10 @@ function CameraBtns({ setIndex }) {
   }
 
   const handleClick = index => {
+    const btns_li = btns.current.querySelectorAll('li');
+    for (const btn of btns_li) btn.classList.remove('on');
+    btns_li[index].classList.add('on');
+
     state.cameraPos.set(...pos[index].cameraPos);
     state.target.set(...pos[index].target);
     state.shouldUpdate = true;
@@ -36,8 +43,16 @@ function CameraBtns({ setIndex }) {
   }
 
   return (
-    <ul className='cameraBtns'>
-      {Array(6).fill().map((_, idx) => <li key={idx} onClick={() => handleClick(idx)}>{`view${idx + 1}`}</li>)}
+    <ul className='cameraBtns' ref={btns}>
+      {Array(6).fill().map((_, idx) => {
+        let isOn = '';
+        if (idx === 0) isOn = 'on';
+
+        return (
+          <li key={idx} className={isOn} onClick={() => handleClick(idx)}>{`view${idx + 1}`}</li>)
+      }
+      )
+      }
     </ul>
   );
 }
